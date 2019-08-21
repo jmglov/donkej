95e57f2eee1f3ab4bcb944c651e4bdfb2c50f52a
Josh Glover <joshua.glover@klarna.com>
2019-08-21 08:53:34 +0200

    date: add units->ms

--- src/donkej/date.cljs
+++ src/donkej/date.cljs
@@ -28,3 +28,24 @@
 
 (defn now-iso-datetime []
   (->iso-datetime (now)))
+
+(defn units->ms [num units]
+  (case units
+    :ms num
+    :millis num
+    :millisecond num
+    :milliseconds num
+    :s (* num 1000)
+    :sec (units->ms num :s)
+    :second (units->ms num :s)
+    :seconds (units->ms num :s)
+    :m (* (units->ms num :s) 60)
+    :min (units->ms num :m)
+    :minute (units->ms num :m)
+    :minutes (units->ms num :m)
+    :h (* (units->ms num :m) 60)
+    :hour (units->ms num :h)
+    :hours (units->ms num :h)
+    :d (* (units->ms num :h) 24)
+    :day (units->ms num :d)
+    :days (units->ms num :d)))
